40bb525fedd40273c38f9a051e37085c02d01982
Richard Neumann <r.neumann@homeinfo.de>
2022-11-11 11:22:29 +0100

    Add "os" key

--- ext_localconf.php
+++ ext_localconf.php
@@ -24,6 +24,7 @@ $extensionKey = 'pinlogin';
         'available' => true,
         'priority' => 100,
         'quality' => 100,
+        'os' => '*',
 
         'className' => 'Homeinfo\\Pinlogin\\Service\\AuthenticationService',
     ]
