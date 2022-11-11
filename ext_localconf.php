9d88625fb798a62814c49bbcab92c14d17c1839f
Richard Neumann <r.neumann@homeinfo.de>
2022-11-11 14:58:30 +0100

    Set exec NULL

--- ext_localconf.php
+++ ext_localconf.php
@@ -23,7 +23,7 @@ defined('TYPO3_MODE') || die();
         'priority' => 100,
         'quality' => 100,
         'os' => 'Any',
-        'exec' => [],
+        'exec' => NULL,
 
         'className' => \Homeinfo\Pinlogin\Service\AuthenticationService::class,
     ]
