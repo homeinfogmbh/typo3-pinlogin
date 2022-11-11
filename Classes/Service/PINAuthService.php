bbd572dd748267663ddbc98f2eedf93e11de4e66
Richard Neumann <r.neumann@homeinfo.de>
2022-11-11 16:04:46 +0100

    Cast record into array

--- Classes/Service/PINAuthService.php
+++ Classes/Service/PINAuthService.php
@@ -69,7 +69,7 @@ final class PINAuthService extends AbstractAuthenticationService
             return FALSE;
         }
         
-        $user = GeneralUtility::makeInstance(FrontendUserRepository::class)
+        $user = (array)GeneralUtility::makeInstance(FrontendUserRepository::class)
             ->findByUid($pin_entries[0]->feuserId);
 
         \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($user, "User record:");
